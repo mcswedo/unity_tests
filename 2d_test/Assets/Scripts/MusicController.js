8f09825434dd6e05ac1e5006b1aff415d5423340
mcswedo <mcswedo@gmail.com>
2013-11-12 16:24:46 -0800

    Created scrolling Note Chart.

---
+++ 2d_test/Assets/Scripts/MusicController.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+@script RequireComponent(AudioSource)
+
+public var beatsPerMinute:float;
+
+public var startTime:double;
+private var secondsPerBeat = 60.0 / beatsPerMinute;
+//private var i = 0;
+public var elapsedTime:double;
+
+//var notes = [];
+
+private var noteChart;
+
+function Start () {
+	noteChart = GameObject.FindGameObjectWithTag("noteChart");
+	
+	audio.Play();
+	startTime = AudioSettings.dspTime;
+	Debug.Log("Start Time: " + startTime);
+	
+}
+
+function Update () {
+	var currentTime = AudioSettings.dspTime;
+	elapsedTime = currentTime - startTime;
+	
+	if (elapsedTime >= secondsPerBeat) {
+		
+		startTime = startTime + secondsPerBeat;
+		
+		Debug.Log("Current Time: " + startTime);
+	}
+}
---
+++ 2d_test/Assets/Scripts/NoteChartController.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+@script RequireComponent(AudioSource)
+
+public var beatsPerMinute:float = 96;
+
+public var secondsPerBeat:float = 60.0 / beatsPerMinute;
+//public var timeSigNum = 6;
+//public var timeSigDenom = 8;
+//public var transformAmt:float = secondsPerBeat * 2 * (timeSigNum / timeSigDenom) / 10;
+
+private var startTime:double;
+private var noteChartTransform:Transform;
+
+function Start () {
+	audio.Play();
+	startTime = AudioSettings.dspTime;
+	Debug.Log("Start Time: " + startTime);	
+	noteChartTransform = GameObject.FindGameObjectWithTag("noteChart").GetComponent(Transform);
+}
+
+function Update () {
+	var currentTime = AudioSettings.dspTime;
+	var elapsedTime = currentTime - startTime;
+	
+	if (elapsedTime >= secondsPerBeat) {
+		startTime = startTime + secondsPerBeat;
+		Debug.Log("Current Time: " + startTime);
+	}
+	
+	//  this transform makes zero sense but let me try to explain.
+	//  transformAmt = secondsPerBeat * 2 * (timeSigNum / timeSigDenom) / 10 = 0.09375.
+	//  Starting with secondsPerBeat, which is .625, the amount of time between beats.
+	//  Multiply by 2 because we are working with 8th notes.
+	//  Multiply by time signature - 6 beats per 8 notes.
+	//  Divide by 10 because our orthographic projection is 10, and we need to keep things small.
+	//  Ugh. I don't know why it doesn't work as formula above, but it will not take it. So gogo magic number.
+	noteChartTransform.position.x -= .09375; 
+}
